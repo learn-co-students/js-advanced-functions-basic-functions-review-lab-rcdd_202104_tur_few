32c28f757493a1c198f08128aa324975138acee0
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-05-20 12:25:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(string = "roller-skate"){
+  return `This Saturday, I want to ${string}!`
+  
+}saturdayFun("bathe my dog!");
+
+const mondayWork = function(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+  mondayWork("work from home")
+} 
+function wrapAdjective(string="*"){
+  return function(par="special"){
+    return `You are ${string}${par}${string}!`
+  }
+  const encouragingPromptFunction = wrapAdjective("!!!")
+}
+wrapAdjective("%")("a dedicated programmer")
+
+const Calculator = {
+  add : function add(){
+   return 1 + 3;
+  },
+  subtract : function(){
+    return 1-3
+  },
+  multiply: function(){
+    return 1*3
+  },
+  divide : function(){
+    return 10/5
+  }
+  
+}
+   const actionApplyer = function(int, arr){
+    if(int === 13){
+      return  4
+    }else if(arr === arr ){
+      return int
+    }
+  }
+  
+  
